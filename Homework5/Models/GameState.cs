5e002059b56b48a56ed1b2c83692968610f6a822
vrtisd <danvrtis@gmail.com>
2018-03-20 20:57:57 -0700

    Added Homework5

---
+++ Homework5/Models/GameState.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Homework5.Models
+{
+    class GameState : INotifyPropertyChanged
+    {
+        //I'm hoping to track the state of the game using this class
+        private int turnNumber;
+        private string playerTurnText;
+        private List<string> winningCombinations;
+        private List<Player> players;
+        private Player currentPlayer;
+
+        public GameState()
+        {
+            turnNumber = 0;
+            players = new List<Player> { new Player("X"), new Player("O") };
+            currentPlayer = players[turnNumber % 2];
+            playerTurnText = currentPlayer.TurnText;
+
+            winningCombinations = new List<string>
+            {
+                "0,0|0,1|0,2",
+                "1,0|1,1|1,2",
+                "2,0|2,1|2,2",
+                "0,0|1,0|2,0",
+                "0,1|1,1|2,1",
+                "0,2|1,2|2,2",
+                "0,0|1,1|2,2",
+                "0,2|1,1|2,0"
+            };
+        }
+
+        public Player CurrentPlayer
+        {
+            get => currentPlayer;
+            set => currentPlayer = players[turnNumber % 2];
+        }
+
+        public int TurnNumber
+        {
+            get => turnNumber;
+            set
+            {
+                if (!PlayerIsWinner())
+                {
+                    turnNumber = value;
+                    currentPlayer = players[turnNumber % 2];
+                    playerTurnText = currentPlayer.TurnText;
+                }
+            }
+        }
+
+        public string PlayerTurnText
+        {
+            get => playerTurnText;
+            set
+            {
+                if (playerTurnText != value)
+                {
+                    playerTurnText = value;
+                    OnPropertyChanged("PlayerTurnText");
+                }
+            }
+        }
+
+        public Player this[string token]
+        {
+            get => players.FirstOrDefault(p => p.PlayerToken.ToString() == token);
+        }
+
+        public Player this[int index]
+        {
+            get
+            {
+                switch (index)
+                {
+                    case 0:
+                    case 1:
+                        return players[index];
+                    default:
+                        return null;
+                }
+            }
+        }
+
+        public Player this[Models.PlayerToken token]
+        {
+            get => players.FirstOrDefault(p => p.PlayerToken == token);
+        }
+
+        public bool PlayerIsWinner()
+        {                        
+            foreach (string winningCombination in winningCombinations)
+            {
+                string[] coordinates = winningCombination.Split('|');
+                
+                if (currentPlayer.SelectedButtons.IndexOf(coordinates[0]) != -1 &&
+                    currentPlayer.SelectedButtons.IndexOf(coordinates[1]) != -1 &&
+                    currentPlayer.SelectedButtons.IndexOf(coordinates[2]) != -1)
+                {
+                    return true;
+                }
+
+            }
+            return false;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Homework5/Models/Player.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework5.Models
+{
+    public enum PlayerToken { X = 0, O = 1}
+
+    class Player : INotifyPropertyChanged
+    {
+        //I'm hoping to track player details using this class
+        private PlayerToken playerToken;
+        private List<string> selectedButtons;
+        private string turnText;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public Player(PlayerToken token)
+        {
+            Initialize(token);
+        }
+
+        public Player(string token)
+        {
+            if(PlayerToken.TryParse(token, out PlayerToken result))
+            {
+                Initialize(result);
+            }
+        }
+
+        private void Initialize(PlayerToken token)
+        {
+            playerToken = token;
+            selectedButtons = new List<string>();
+            turnText = $"{token.ToString()}'s turn";
+        }
+
+        public PlayerToken PlayerToken
+        {
+            get => playerToken;
+            set => playerToken = value;
+        }
+
+        public List<string> SelectedButtons
+        {
+            get => selectedButtons;
+            set => selectedButtons = value;
+        }
+
+        public string TurnText
+        {
+            get => turnText;
+            private set => turnText = value;
+        }
+    }
+}

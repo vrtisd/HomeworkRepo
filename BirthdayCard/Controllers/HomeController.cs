ec39dbdece77701ba5fbe970ea2c42117d9d98cd
vrtisd <danvrtis@gmail.com>
2018-04-19 16:37:55 -0700

    Added BirthdayCard

---
+++ BirthdayCard/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BirthdayCard.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult CardForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CardForm(Models.Card card)
+        {
+            if(ModelState.IsValid)
+            {
+                return View("CardSent", card);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ BirthdayCard/Models/Card.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace BirthdayCard.Models
+{
+    public class Card
+    {
+        [Required(ErrorMessage = "Please enter \"From\" name")]
+        public string From { get; set; }
+
+        [Required(ErrorMessage = "Please enter \"To\" name")]
+        public string To { get; set; }
+
+        [Required(ErrorMessage = "Please enter a message")]
+        public string Message { get; set; }
+    }
+}
